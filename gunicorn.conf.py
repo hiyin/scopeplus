5d97bfef00000d6213555525a4afef1da7662617
Angela Yin <hiyin@gmail.com>
2022-09-27 02:44:08 +0000

    add ssl

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,5 @@
 preload_app=False
 accesslog = "/home/d24h_prog5/flasklog/gunicorn_access_new.log"
 errorlog = "/home/d24h_prog5/flasklog/gunicorn_error_new.log"
+certfile = "/etc/ssl/certs/ssl-cert-snakeoil.pem"
+keyfile = "/etc/ssl/private/ssl-cert-snakeoil.key"
